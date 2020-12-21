bf48943d651fef19facac0c38b3f0ad33665d13a
amol kapoor <theahura@gmail.com>
2020-12-21 15:31:08 -0500

    Added frontend template and rendering

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify, render_template, request
+app = Flask(__name__, template_folder='frontend/', static_folder='frontend/')
 
-@app.route('/')
-def hello_world():
-  return 'hello world'
+@app.route('/', methods=['GET'])
+def index():
+  return render_template('index.html')
 
 def main():
   app.run(debug=True)
