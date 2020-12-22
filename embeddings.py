6d3b00dae2a4c0b0fb03fdc8c21afba12e144d13
amol kapoor <theahura@gmail.com>
2020-12-21 22:35:59 -0500

    Added embeddings code.

---
+++ embeddings.py
@@ -0,0 +1,51 @@
+import glob
+import time
+
+import numpy as np
+import pandas as pd
+from tensorflow.keras.applications import InceptionV3
+from tensorflow.keras.applications.inception_v3 import preprocess_input
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+
+
+def stream_images(paths):
+  '''Read in all images from args[0], a list of image paths'''
+  for idx, path in enumerate(paths):
+    try:
+      yield load_img(path)
+    except Exception as exc:
+      print('Image at path ', path, 'could not be processed --', exc)
+
+
+def get_image_embeddings(paths):
+  # Load the model. Currently using an Inception model but this is probably
+  # slow, see if we can use a distilled model.
+  base = InceptionV3(include_top=True, weights='imagenet',)
+  model = Model(inputs=base.input, outputs=base.get_layer('avg_pool').output)
+
+  def get_embedding_(path):
+    try:
+      image = load_img(path)
+    except Exception as exc:
+      print('Image at path ', path, 'could not be processed --', exc)
+      return None
+    preprocessed_image = preprocess_input(
+      img_to_array(image.resize((299, 299))))
+    return model.predict(np.expand_dims(preprocessed_image, 0)).squeeze()
+
+  paths_df = pd.Series(paths)
+
+  start = time.time()
+  embeddings = paths_df.map(get_embedding_).dropna()
+  stop = time.time()
+
+  print("Time taken: ", stop - start)
+  return embeddings.to_numpy()
+
+
+if __name__ == '__main__':
+  DATA_PATH = '/home/amol/Code/repos/high-dimensional-data-viz/data/*/*'
+  file_names = glob.glob(DATA_PATH)
+  embeddings = get_image_embeddings(file_names)
+  import IPython; IPython.embed()
--- server.py
+++ server.py
@@ -1,12 +1,31 @@
 from flask import Flask, jsonify, render_template, request
+
+UPLOAD_FOLDER = '~/tmp/'
+
 app = Flask(__name__, template_folder='frontend/', static_folder='frontend/')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/', methods=['GET'])
 def index():
   return render_template('index.html')
 
+
+@app.route('/upload', methods=['POST'])
+def upload():
+  # Handle file loading here. For now, just return the base path with all of
+  # the data.
+  pass
+
+
 def main():
   app.run(debug=True)
+
+
+# Upload images.
+#   Get embeddings for each image.
+#   Create graph.
+#   Return graph in d3--interpretable format (streaming?).
 
 if __name__ == '__main__':
   main()
